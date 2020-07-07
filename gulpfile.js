30cf7a8aa4a534e08a021e24b3bce7d65b75cc80
irene-gomez <irene@irenegf.com>
2020-07-07 17:53:29 +0200

    Add new gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,22 @@ const htmlInclude = require('gulp-html-tag-include');
 const browserSync = require('browser-sync');
 const del = require('del');
 
+gulp.task('bs-reload', done => {
+    browserSync.reload();
+    done();
+});
+
 gulp.task('clean', del.bind(null, [config.env.dev.dest]));
+
+gulp.task('clean-dist', del.bind(null, [config.env.producction.dest]));
 
 gulp.task('html', done => {
     gulp.src(config.html.src).pipe(htmlInclude()).pipe(gulp.dest(config.html.dest));
+    done();
+});
+
+gulp.task('html-dist', done => {
+    gulp.src(config.html.src).pipe(htmlInclude()).pipe(gulp.dest(config.html.dist));
     done();
 });
 
