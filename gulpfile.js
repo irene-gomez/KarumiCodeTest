c33ab82080a5dc8900771fd87feb9767f715f7ec
irene-gomez <irene@irenegf.com>
2020-07-08 17:59:58 +0200

    Update gulpfile with images tasks and .gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,8 @@ const notify = require('gulp-notify');
 const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('bs-reload', done => {
-    browserSync.reload();
-    done();
+	browserSync.reload();
+	done();
 });
 
 gulp.task('clean', del.bind(null, [config.env.dev.dest]));
@@ -19,54 +19,72 @@ gulp.task('clean', del.bind(null, [config.env.dev.dest]));
 gulp.task('clean-dist', del.bind(null, [config.env.producction.dest]));
 
 gulp.task('html', done => {
-    gulp.src(config.html.src).pipe(htmlInclude()).pipe(gulp.dest(config.html.dest));
-    done();
+	gulp.src(config.html.src).pipe(htmlInclude()).pipe(gulp.dest(config.html.dest));
+	done();
 });
 
 gulp.task('html-dist', done => {
-    gulp.src(config.html.src).pipe(htmlInclude()).pipe(gulp.dest(config.html.dist));
-    done();
+	gulp.src(config.html.src).pipe(htmlInclude()).pipe(gulp.dest(config.html.dist));
+	done();
+});
+
+gulp.task('images', done => {
+	gulp
+		.src(config.images.src)
+		.pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+		.pipe(gulp.dest(config.images.dest));
+	done();
 });
 
 gulp.task('css', done => {
-    gulp
-        .src(config.css.src)
-        .pipe(sourcemaps.init())
-        .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
-        .pipe(sass({ outputStyle: 'extended' }))
-        .pipe(autoprefixer({ cascade: false }))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(config.css.dest))
-        .pipe(browserSync.reload({ stream: true }));
-    done();
+	gulp
+		.src(config.css.src)
+		.pipe(sourcemaps.init())
+		.pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+		.pipe(sass({ outputStyle: 'extended' }))
+		.pipe(autoprefixer({ cascade: false }))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(config.css.dest))
+		.pipe(browserSync.reload({ stream: true }));
+	done();
 });
 
 gulp.task('css-dist', done => {
-    gulp
-        .src(config.css.src)
-        .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
-        .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(autoprefixer({ cascade: false }))
-        .pipe(gulp.dest(config.css.dist));
-    done();
+	gulp
+		.src(config.css.src)
+		.pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+		.pipe(sass({ outputStyle: 'compressed' }))
+		.pipe(autoprefixer({ cascade: false }))
+		.pipe(gulp.dest(config.css.dist));
+	done();
+});
+
+gulp.task('images-dist', done => {
+	gulp
+		.src(config.images.src)
+		.pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+		.pipe(gulp.dest(config.images.dist));
+	done();
 });
 
 gulp.task('default',
-    gulp.series(['clean', 'html', 'css'], done => {
-        browserSync.init({ server: { baseDir: './public/' } });
-        gulp.watch(config.watch.html, gulp.series(['html', 'bs-reload']));
-        gulp.watch(config.css.src, gulp.series('css'));
-        done();
-    })
+	gulp.series(['clean', 'html', 'css', 'images'], done => {
+		browserSync.init({ server: { baseDir: './public/' } });
+		gulp.watch(config.watch.html, gulp.series(['html', 'bs-reload']));
+		gulp.watch(config.css.src, gulp.series('css'));
+		gulp.watch(config.images.src, gulp.series(['images', 'bs-reload']));
+		done();
+	})
 );
 
 gulp.task('docs',
-    gulp.series(
-        [
-            'clean-dist',
-            'html-dist',
-            'css-dist'
-        ],
-        done => done()
-    )
+	gulp.series(
+		[
+			'clean-dist',
+			'html-dist',
+			'css-dist',
+			'images-dist'
+		],
+		done => done()
+	)
 );
